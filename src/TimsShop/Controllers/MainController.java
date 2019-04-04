c572d07deb2e87d6ae9b59cfc9956cec64c762a2
Alexjp <alnerdo@hotmail.com>
2019-04-04 21:30:35 +1100

    * Added blank  class files in Model Folder/

--- src/TimsShop/Controllers/MainController.java
+++ src/TimsShop/Controllers/MainController.java
@@ -1,55 +1,27 @@
 
 package TimsShop.Controllers;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
 /****************************************************************
 This class serves as controller between the views of  Tims Toy shop.
 NOte: Any @FXML annotations denote XML loaded components/listeners
-* To edit the GUI element, install scene builder @
+    1 Constructs an instance of the specified Application class
+    2 Calls the init() method
+    3 Calls the start(javafx.stage.Stage) method
+    4 Waits for the application to finish, which happens when either of the following occur:
+        * the application calls Platform.exit()
+        * the last window has been closed and the implicitExit attribute on Platform is true
+    5 Calls the stop() method
+    * 
+    * 
 ********************************************************************/
-public class MainController extends Application implements Initializable 
+public class MainController extends Application  // Application class serves as the Entry point to a FX program.
 {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) 
-    {
-        label.setText("Hello World!");
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-
-    /**********************************************************************
-    Function: called to initialize a controller after its 
-              root element has been completely processed.
-    Arguments:  @param url locator for FXML document.
-                @param rb bundles the locally configured language settings
-    ************************************************************************/
-    @Override
-    public void initialize(URL url, ResourceBundle rb) 
-    {
-       
-    }    
      /**********************************************************************
      Function: loads XML document and instantiates the main Stage (view) 
      Arguments: the initial frame of the program
@@ -65,6 +37,16 @@ public class MainController extends Application implements Initializable
         primaryStage.show();
     }
     
+    
+    
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
         launch(args);
