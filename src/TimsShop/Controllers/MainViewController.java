25fc245f8511cf850e27a92381dae8cae060679c
Alexjp <alnerdo@hotmail.com>
2019-05-14 15:53:05 +1000

    Added filtered search, "re-factored" methods (split functions/use of lambdas)

--- src/TimsShop/Controllers/MainViewController.java
+++ src/TimsShop/Controllers/MainViewController.java
@@ -8,8 +8,11 @@ import TimsShop.Controllers.Dialogs.AddToyDialog;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -18,15 +21,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 
+/**************************************************************
+ * The MainViewController 
+ */
 public class MainViewController implements  Initializable  
 {
         
@@ -42,13 +47,22 @@ public class MainViewController implements  Initializable
     private Button insertButton;
     @FXML
     private Button logoutButton;
+    @FXML
+    private Button addCategoryButton;
     
-    // Table components
+    /**************TABLE COMPONENTS********************/
     @FXML 
     private TableView toyTable;
+    private TableColumn<Toy, Long> idCol;
+    private TableColumn<Toy, String> nameCol;
+    private TableColumn<Toy, String> priceCol;
+    private TableColumn<Toy, String> categoryCol;
+    private ObservableList<Toy> toyData;
     
-    // Data storage for the application
-    ShopDataStorage storage;
+    
+    //Data storage for the application
+    private ShopDataStorage storage;
+
     
     /**********************************************************************
     Function: called to initialize a controller after its 
@@ -58,48 +72,38 @@ public class MainViewController implements  Initializable
     ************************************************************************/
     @Override
     public void initialize(URL url, ResourceBundle rb) 
-    {
-        // Initialize the data storage engine. It will create a database if it doesn't exist yet.
-        storage = new ShopDataStorage();
-        
-        toyTable.setItems(storage.getToys());
-        
-        TableColumn<Toy, Long> idCol = new TableColumn<Toy, Long>("Id");
-        TableColumn<Toy, String> nameCol = new TableColumn<Toy, String>("Name");
-        TableColumn<Toy, String> priceCol = new TableColumn<Toy, String>("Price");
-        TableColumn<Toy, String> categoryCol = new TableColumn<Toy, String>("Category");
-        
-        idCol.setCellValueFactory(new Callback<CellDataFeatures<Toy, Long>, ObservableValue<Long>>() {
-            @Override
-            public ObservableValue<Long> call(CellDataFeatures<Toy, Long> p) {
-                return new ReadOnlyObjectWrapper<Long>(p.getValue().getId());
-            }
-         });
-        
-        nameCol.setCellValueFactory(new Callback<CellDataFeatures<Toy, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(CellDataFeatures<Toy, String> p) {
-                return new ReadOnlyObjectWrapper<String>(p.getValue().getName());
-            }
-         });
-        
-        priceCol.setCellValueFactory(new Callback<CellDataFeatures<Toy, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(CellDataFeatures<Toy, String> p) {
-                return new ReadOnlyObjectWrapper<String>(String.format("$%.2f", p.getValue().getPrice()));
-            }
-         });
-        
-        categoryCol.setCellValueFactory(new Callback<CellDataFeatures<Toy, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(CellDataFeatures<Toy, String> p) {
-                return new ReadOnlyObjectWrapper<String>(storage.getCategoryById(p.getValue().getCategoryId()).getName());
-            }
-         });
-        
-        toyTable.getColumns().addAll(idCol, nameCol, priceCol, categoryCol);
+    { 
+        bindData();
+        setTableHeaders();
+        setTableData();
     }
     
+    /***********************TABLE INITIALIZATION*******************************/
+    private void bindData()
+    {   // Initialize the data storage engine. It will create a database if it doesn't exist yet.
+        storage = new ShopDataStorage();
+        toyData = storage.getToys();
+        toyTable.setItems(toyData);
+     }
+  
+    private void setTableHeaders()
+    {
+        idCol = new TableColumn<>("Id");
+        nameCol = new TableColumn<>("Name");
+        priceCol = new TableColumn<>("Price");
+        categoryCol = new TableColumn<>("Category");
+    }
+    private void setTableData()
+    {
+        idCol.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(p.getValue().getId()));
+        nameCol.setCellValueFactory( p -> new ReadOnlyObjectWrapper<>(p.getValue().getName()));
+        priceCol.setCellValueFactory(p -> new ReadOnlyObjectWrapper<>(String.format("$%.2f", p.getValue().getPrice())));
+        categoryCol.setCellValueFactory( p -> new ReadOnlyObjectWrapper<>(storage.getCategoryById(p.getValue().getCategoryId()).getName()));
+        toyTable.getColumns().addAll(idCol, nameCol, priceCol, categoryCol);
+    }
+
+    
+    /*******************************EVENT LISTENERS****************************/
     public void onClose() {
         System.out.println("Saving database...");
         storage.write();
@@ -132,7 +136,7 @@ public class MainViewController implements  Initializable
         }
         
         // Function to launch the dialog to insert certain items into the database
-        FXMLLoader toyDialogLoader = new FXMLLoader(getClass().getResource("/TimsShop/FXML/InsertToyDialog.fxml"));
+        FXMLLoader toyDialogLoader = new FXMLLoader(getClass().getResource("/TimsShop/Views/InsertToyDialog.fxml"));
         Parent toyDialogRoot = toyDialogLoader.load();
         AddToyDialog dialog = toyDialogLoader.<AddToyDialog>getController();
         
@@ -150,21 +154,48 @@ public class MainViewController implements  Initializable
     @FXML
     private void logoutHandler(MouseEvent event) throws IOException
     {
-             /*********************************
-                TODO:
-                 * Report staff member logged in 
-                 * Close login stage- DONE
-            **********************************/
-            //Close login stage
-            Stage currentStage = (Stage)logoutButton.getScene().getWindow();
-            currentStage.close();
-           
-            //Load Main Stage FXML
-            Stage loginStage = new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("/TimsShop/FXML/EmployeeLoginView.fxml"));
-            Scene scene = new Scene(root);
-            loginStage.setScene(scene);
-            loginStage.show();
+         /*********************************
+            TODO:
+             * Report staff member logged in 
+             * Close login stage- DONE
+        **********************************/
+        //Close login stage
+        Stage currentStage = (Stage)logoutButton.getScene().getWindow();
+        currentStage.close();
+
+        //Load Main Stage FXML
+        Stage loginStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/TimsShop/Views/EmployeeLoginView.fxml"));
+        Scene scene = new Scene(root);
+        loginStage.setScene(scene);
+        loginStage.show();
     }
+
+    @FXML
+    private void handleSearchEntry(KeyEvent event)
+    {
+        //Add Data to list based on filter
+        FilteredList<Toy> filteredData = new FilteredList<>(toyData,  e -> true);
+        
+        //Listener compares table data to  string value entered by user in search bar
+        searchBar.textProperty().addListener((observableValue, oldValue, newValue) -> 
+        {   
+            filteredData.setPredicate((Predicate<? super Toy>) toy -> 
+            {   //If search bar is empty, no filter is applied
+                if(newValue == null || newValue.isEmpty())
+                {
+                    return true;
+                }
+                //Check search string is contained within name of toy
+                return toy.getName().toLowerCase().contains(newValue.toLowerCase());
+            });
+            //Sort the filtered list based on insertion order, then bind the list to the table
+            SortedList<Toy> sortedData = new SortedList<>(filteredData);
+            sortedData.comparatorProperty().bind(toyTable.comparatorProperty());
+            toyTable.setItems(sortedData);
+        });
+        
+    }
+    
     
 }
